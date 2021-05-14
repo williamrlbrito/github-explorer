f5b82b222fad9bf3c967b69ecea2cf88dd22cfbb
William Brito <williamrldb@gmail.com>
2021-05-14 14:34:52 -0400

    --Eslint, Prettier e Editor Config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
-import Routes from './routes'
+import Routes from './routes';
 
 const App: React.FC = () => (
   <BrowserRouter>
     <Routes />
   </BrowserRouter>
-) ;
+);
 
 export default App;
