9249a628b2ac7ae56f6d283d132f01bae1ded5f1
William Brito <williamrldb@gmail.com>
2021-05-15 10:21:16 -0400

    --Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,64 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
 
-const Dashboard: React.FC = () => <Title>Explore repositories no Github</Title>;
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="Githun Explorer" />
+    <Title>Explore repositories no Github</Title>
+
+    <Form>
+      <input placeholder="Digite aqui" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="teste">
+        <img
+          src="https://avatars.githubusercontent.com/u/19253614?v=4"
+          alt="William Brito"
+        />
+
+        <div>
+          <strong>Githun Explorer</strong>
+          <p>Explore repositories no Github</p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="teste">
+        <img
+          src="https://avatars.githubusercontent.com/u/19253614?v=4"
+          alt="William Brito"
+        />
+
+        <div>
+          <strong>Githun Explorer</strong>
+          <p>Explore repositories no Github</p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="teste">
+        <img
+          src="https://avatars.githubusercontent.com/u/19253614?v=4"
+          alt="William Brito"
+        />
+
+        <div>
+          <strong>Githun Explorer</strong>
+          <p>Explore repositories no Github</p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
